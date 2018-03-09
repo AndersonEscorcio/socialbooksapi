aa7dcc435a6d4efe8e7f05f19e91506d41143577
AndersonEscorcio <37222507+AndersonEscorcio@users.noreply.github.com>
2018-03-09 18:20:18 +0000

    Update SocialbooksapiApplicationTests.java

--- socialbooksapi/src/test/java/com/algaworks/socialbooks/SocialbooksapiApplicationTests.java
+++ socialbooksapi/src/test/java/com/algaworks/socialbooks/SocialbooksapiApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/*Teste atualizado no GitHub*/
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SocialbooksapiApplicationTests {
