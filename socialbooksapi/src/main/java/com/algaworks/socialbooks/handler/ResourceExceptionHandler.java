abf7fa5f7f7cc65a998680be706fc9b89b742383
AndersonEscorcio <anderson.escorcio82@gmail.com>
2018-03-09 20:17:22 -0300

    Commit projeto SocialBooksApi 09/03/18 20:17

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/handler/ResourceExceptionHandler.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/handler/ResourceExceptionHandler.java
@@ -2,19 +2,28 @@ package com.algaworks.socialbooks.handler;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.algaworks.socialbooks.domain.DetalhesErro;
 import com.algaworks.socialbooks.services.exceptions.LivroNaoEncontradoException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(LivroNaoEncontradoException.class)
-	public ResponseEntity<Void> handleLivroNaoEncontradoException
+	@ExceptionHandler(LivroNaoEncontradoException.class) //Exceção retornada dessa classe
+	public ResponseEntity<DetalhesErro> handleLivroNaoEncontradoException
 									(LivroNaoEncontradoException e, HttpServletRequest request){
-		return ResponseEntity.notFound().build();
+		
+		DetalhesErro erro = new DetalhesErro();
+		erro.setStatus(404l);		
+		erro.setTitulo("O livro não pôde ser encontrado.");
+		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/404");
+		erro.setTimestamp(System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
 	}
 									
 	
